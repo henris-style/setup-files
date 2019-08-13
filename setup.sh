943b415f3665f83c372bbd2a098dd89c68bee1c2
Sil van Diepen <me@silvandiepen.com>
2019-08-13 21:02:00 +0200

    fix spaces

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 URL="https://raw.githubusercontent.com/henris-style/setup-files/master"
 
 curl -o app.scss -l ${URL}/app.scss
-printf '\n\tApp.scss  \e[1;34mcreated!.\e[0m' 
+printf '\n\tApp.scss \e[1;34mcreated!\e[0m' 
 
 # tools
 mkdir tools 
@@ -11,7 +11,7 @@ curl -o tools.scss -l ${URL}/tools.scss
 curl -o tools/_pre.scss -l ${URL}/tools/_pre.scss 
 curl -o tools/_post.scss -l ${URL}/tools/_post.scss 
 
-printf '\n\tTools  \e[1;34mcreated!.\e[0m' 
+printf '\n\tTools \e[1;34mcreated!\e[0m' 
 
 # typography
 mkdir typography 
@@ -21,14 +21,14 @@ curl -o typography/_embed.scss -l ${URL}/typography/_embed.scss
 curl -o typography/_spacing.scss -l ${URL}/typography/_spacing.scss 
 curl -o typography/_usage.scss -l ${URL}/typography/_usage.scss 
 
-printf '\n\tTypography  \e[1;34mcreated!.\e[0m' 
+printf '\n\tTypography \e[1;34mcreated!\e[0m' 
 
 #components
 mkdir components 
 
 curl -o components/_index.scss -l ${URL}/components/_index.scss 
 
-printf '\n\tComponents \e[1;34mcreated!.\e[0m'
+printf '\n\tComponents \e[1;34mcreated!\e[0m'
 
 #colors
 mkdir color 
@@ -36,5 +36,5 @@ mkdir color
 curl -o color/_index.scss -l ${URL}/color/_index.scss 
 curl -o color/_custom-colors.scss -l ${URL}/color/_custom-colors.scss 
 
-printf '\n\tColors \e[1;34mcreated!.\e[0m'
+printf '\n\tColors \e[1;34mcreated!\e[0m'
 printf '\n\t'
