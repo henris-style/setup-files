d581ac467d0c57cc30501635396e6e57f9783358
Sil van Diepen <me@silvandiepen.com>
2019-08-15 11:14:27 +0200

    keep silent on success, but error on fail

--- setup.sh
+++ setup.sh
@@ -8,45 +8,45 @@ if [ -z "$1" ]
 		exit 1
 fi
 
-curl -o ${TARGET}app.scss -l ${URL}/src/app.scss --silent
+curl -o ${TARGET}app.scss -l ${URL}/app.scss --fail --silent --show-error
 printf '\n  App.scss \e[1;34mcreated!\e[0m' 
 
 # tools
 mkdir -p ${TARGET}tools 
 
-curl -o ${TARGET}tools.scss -l ${URL}/src/tools.scss --silent
-curl -o ${TARGET}tools/_pre.scss -l ${URL}/src/tools/_pre.scss --silent
-curl -o ${TARGET}tools/_post.scss -l ${URL}/src/tools/_post.scss --silent
+curl -o ${TARGET}tools.scss -l ${URL}/tools.scss --fail --silent --show-error
+curl -o ${TARGET}tools/_pre.scss -l ${URL}/tools/_pre.scss --fail --silent --show-error
+curl -o ${TARGET}tools/_post.scss -l ${URL}/tools/_post.scss --fail --silent --show-error
 
 printf '\n  Tools \e[1;34mcreated!\e[0m' 
 
 # typography
 mkdir -p ${TARGET}typography 
 
-curl -o ${TARGET}typography/_index.scss -l ${URL}/src/typography/_index.scss --silent
-curl -o ${TARGET}typography/_embed.scss -l ${URL}/src/typography/_embed.scss --silent
-curl -o ${TARGET}typography/_spacing.scss -l ${URL}/src/typography/_spacing.scss --silent
-curl -o ${TARGET}typography/_usage.scss -l ${URL}/src/typography/_usage.scss --silent
+curl -o ${TARGET}typography/_index.scss -l ${URL}/typography/_index.scss --fail --silent --show-error
+curl -o ${TARGET}typography/_embed.scss -l ${URL}/typography/_embed.scss --fail --silent --show-error
+curl -o ${TARGET}typography/_spacing.scss -l ${URL}/typography/_spacing.scss --fail --silent --show-error
+curl -o ${TARGET}typography/_usage.scss -l ${URL}/typography/_usage.scss --fail --silent --show-error
 
 printf '\n  Typography \e[1;34mcreated!\e[0m' 
 
 #components
 mkdir -p ${TARGET}components 
 
-curl -o ${TARGET}components/_index.scss -l ${URL}/src/components/_index.scss --silent
+curl -o ${TARGET}components/_index.scss -l ${URL}/components/_index.scss --fail --silent --show-error
 
 printf '\n  Components \e[1;34mcreated!\e[0m'
 
 #colors
 mkdir -p ${TARGET}color 
 
-curl -o ${TARGET}color/_index.scss -l ${URL}/src/color/_index.scss --silent
+curl -o ${TARGET}color/_index.scss -l ${URL}/color/_index.scss --fail --silent --show-error
 
 if [ "$2" == "guyn" ]
   then
-		curl -o ${TARGET}color/_custom-colors.scss -l ${URL}/src/color/_custom-colors-guyn.scss --silent
+	curl -o ${TARGET}color/_custom-colors.scss -l ${URL}/color/_custom-colors-guyn.scss --fail --silent --show-error
 else
-		curl -o ${TARGET}color/_custom-colors.scss -l ${URL}/src/color/_custom-colors.scss --silent
+	curl -o ${TARGET}color/_custom-colors.scss -l ${URL}/color/_custom-colors.scss --fail --silent --show-error
 fi
 
 printf '\n  Colors \e[1;34mcreated!\e[0m'
